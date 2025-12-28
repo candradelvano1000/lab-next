727d2351cfa9f228f76044b15a5f527e96cce9cc
Candra Delvano <candra@neuronworks.co.id>
2025-12-28 13:10:06 +0700

    push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
